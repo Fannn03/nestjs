4a8061f9bf133fd201c275f6c56293a09ccd5f65
Fannn03 <fann.fann771@gmail.com>
2023-11-07 21:45:24 +0700

    build: generate titles from nest generate resouces

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TitlesModule } from './titles/titles.module';
 
 @Module({
-  imports: [],
+  imports: [TitlesModule],
   controllers: [AppController],
   providers: [AppService],
 })
